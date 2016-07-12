1a624791b9272a1a487ac36197f041725b83a30e
Tatsuya Ono <ononoma@gmail.com>
2016-07-12 13:37:33 +0100

    Install httpoison

--- mix.exs
+++ mix.exs
@@ -14,7 +14,12 @@ defmodule FrameworkStats.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [
+      applications: [
+        :logger,
+        :httpoison
+      ]
+    ]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +32,8 @@ defmodule FrameworkStats.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      { :httpoison, "~> 0.9" }
+    ]
   end
 end
