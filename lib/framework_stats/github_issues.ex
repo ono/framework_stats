ea1d7895f09207988b21415e09157b71bac82f05
Tatsuya Ono <ononoma@gmail.com>
2016-07-12 15:12:53 +0100

    Count github issues

--- lib/framework_stats/github_issues.ex
+++ lib/framework_stats/github_issues.ex
@@ -1,10 +1,43 @@
 defmodule FrameworkStats.GithubIssues do
+  @headers [ { "User-agent", "github.com/ono/framework_stats" } ]
 
-  def get_count(path) do
+  def get_stats(repo) do
+    open = get_count(repo, "open")
+    all = get_count(repo, "all")
 
+    %{open_issues: open, all_issues: all}
   end
 
-  def issue_url(path, status) do
+  def get_count(repo, state) do
+    issues_url(repo, state)
+    |> HTTPoison.get(@headers)
+    |> retrieve_link_header
+    |> retrieve_count
+  end
 
+  defp issues_url(repo, state) do
+    "https://api.github.com/repos/#{repo}/issues?state=#{state}&per_page=1"
+  end
+
+  defp retrieve_count({"Link", link}) do
+    # Take 25771 from...
+    # [["page=1", "1"], ["page=2", "2"], ["page=1", "1"], ["page=25771","25771"]]
+    {count, _} = Regex.scan(~r/page=([0-9]+)/, link)
+      |> Enum.at(-1)
+      |> Enum.at(-1)
+      |> Integer.parse
+    count
+  end
+
+  defp retrieve_count(_) do
+    0
+  end
+
+  defp retrieve_link_header({:ok, %{status_code: 200, headers: headers}}) do
+    Enum.find(headers, fn(h) -> elem(h, 0) == "Link" end)
+  end
+
+  defp retrieve_link_header(_) do
+    nil
   end
 end
